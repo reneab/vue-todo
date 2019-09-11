fea0936dfa207ac62d617967c0ee2c08f719dd14
reneab <rene.abd@gmail.com>
2019-09-11 16:57:35 +0800

    add message when list is empty or all are completed

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
-        <!-- TODO: Display message if list is empty -->
         <div v-for="todo in list" v-bind:key="todo.id">
             <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+        </div>
+        <div v-if="list.length == 0 || list.filter(e => e.completed == false).length == 0">
+            <h3>Congrats! You have no todos left</h3>
         </div>
     </div>
 </template>
