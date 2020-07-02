443063b5e61b8771847a12c612e3d72f07b1b0c1
reneab <rene.abd@gmail.com>
2020-07-02 21:06:39 +0800

    fixed error with console and added a default todo

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js Todo App" />
-    <AddTodo v-on:add-todo="addItem"/>
-    <Todos v-bind:list="todos" v-on:del-todo="deleteItem"/>
+    <AddTodo v-on:add-todo="addItem" />
+    <p class="error">{{ errorMsg }}</p>
+    <Todos v-bind:list="todos" v-on:del-todo="deleteItem" />
   </div>
 </template>
 
@@ -11,7 +12,8 @@
 import HelloWorld from "./components/HelloWorld.vue";
 import Todos from "./components/Todos.vue";
 import AddTodo from "./components/AddTodo.vue";
-import axios from 'axios'; // npm library for HTTP requests 
+import axios from "axios"; // npm library for HTTP requests
+import uuid from 'uuid';
 
 export default {
   name: "app",
@@ -22,7 +24,14 @@ export default {
   },
   data() {
     return {
-      todos: []
+      todos: [
+        {
+          id: uuid.v4(),
+          title: "A default, hard-coded todo",
+          completed: false
+        }
+      ],
+      errorMsg: ""
     };
   },
   methods: {
@@ -33,10 +42,12 @@ export default {
       this.todos.push(newTodo);
     }
   },
-  created() { // equivalent to init method
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=3')
-      .then(res => this.todos = res.data)
-      .catch(err => console.error(err))
+  created() {
+    // equivalent to init method
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=3")
+      .then(res => res.data.forEach(e => this.todos.push(e)))
+      .catch(err => this.errorMsg = err);
   }
 };
 </script>
@@ -54,4 +65,6 @@ export default {
 img {
   height: 50px;
 }
+
+.error { color: crimson}
 </style>
